2eb40be33677afadb0aab7f5262eacbd02eab45c
maciej022 <maciejpieper56@gmail.com>
2025-04-15 20:50:15 +0200

    Dodanie poziomow trudnosci , rozmiar map daiamenty (do dokonaczenia)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,73 +10,97 @@ namespace Gra2D
 {
     public partial class MainWindow : Window
     {
-        // Stałe reprezentujące rodzaje terenu
-        public const int LAS = 1;     // las
-        public const int LAKA = 2;     // łąka
-        public const int SKALA = 3;   // skały
-        public const int ILE_TERENOW = 4;   // ile terenów
-        // Mapa przechowywana jako tablica dwuwymiarowa int
+        public const int LAS = 1;
+        public const int LAKA = 2;
+        public const int SKALA = 3;
+        public const int DIAMENT = 4; // Dodanie nowego typu - diament
+        public const int ILE_TERENOW = 5;
+
         private int[,] mapa;
         private int szerokoscMapy;
         private int wysokoscMapy;
-        // Dwuwymiarowa tablica kontrolek Image reprezentujących segmenty mapy
         private Image[,] tablicaTerenu;
-        // Rozmiar jednego segmentu mapy w pikselach
         private const int RozmiarSegmentu = 32;
 
-        // Tablica obrazków terenu – indeks odpowiada rodzajowi terenu
-        // Indeks 1: las, 2: łąka, 3: skały
         private BitmapImage[] obrazyTerenu = new BitmapImage[ILE_TERENOW];
-
-        // Pozycja gracza na mapie
         private int pozycjaGraczaX = 0;
         private int pozycjaGraczaY = 0;
-        // Obrazek reprezentujący gracza
         private Image obrazGracza;
-        // Licznik zgromadzonego drewna
         private int iloscDrewna = 0;
+        private int iloscDiamentow = 0; // Licznik diamentów
+        private string wybranyRozmiar = "mala";
+        private string trudnosc = "latwy";
+
         public MainWindow()
         {
             InitializeComponent();
             WczytajObrazyTerenu();
 
-            // Inicjalizacja obrazka gracza
             obrazGracza = new Image
             {
                 Width = RozmiarSegmentu,
                 Height = RozmiarSegmentu
             };
-            BitmapImage bmpGracza = new BitmapImage(new Uri("gracz.png", UriKind.Relative));
+
+            BitmapImage bmpGracza = new BitmapImage();
+            bmpGracza.BeginInit();
+            bmpGracza.UriSource = new Uri("gracz.png", UriKind.Relative);
+            bmpGracza.EndInit();
             obrazGracza.Source = bmpGracza;
         }
+
         private void WczytajObrazyTerenu()
         {
-            // Zakładamy, że tablica jest indeksowana od 0, ale używamy indeksów 1-3
             obrazyTerenu[LAS] = new BitmapImage(new Uri("las.png", UriKind.Relative));
             obrazyTerenu[LAKA] = new BitmapImage(new Uri("laka.png", UriKind.Relative));
             obrazyTerenu[SKALA] = new BitmapImage(new Uri("skala.png", UriKind.Relative));
+            obrazyTerenu[DIAMENT] = new BitmapImage(new Uri("diament.jpg", UriKind.Relative)); // Obrazek diamentu z .jpg
         }
 
-        // Wczytuje mapę z pliku tekstowego i dynamicznie tworzy tablicę kontrolek Image
         private void WczytajMape(string sciezkaPliku)
         {
             try
             {
-                var linie = File.ReadAllLines(sciezkaPliku);//zwraca tablicę stringów, np. linie[0] to pierwsza linia pliku
+                var linie = File.ReadAllLines(sciezkaPliku);
                 wysokoscMapy = linie.Length;
-                szerokoscMapy = linie[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;//zwraca liczbę elementów w tablicy
+                szerokoscMapy = linie[0].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
                 mapa = new int[wysokoscMapy, szerokoscMapy];
 
                 for (int y = 0; y < wysokoscMapy; y++)
                 {
-                    var czesci = linie[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);//zwraca tablicę stringów np. czesci[0] to pierwszy element linii
+                    var czesci = linie[y].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                     for (int x = 0; x < szerokoscMapy; x++)
                     {
-                        mapa[y, x] = int.Parse(czesci[x]);//wczytanie mapy z pliku
+                        int teren = int.Parse(czesci[x]);
+
+                        // Zapobiegamy umieszczaniu skał na pozycji (0, 0) oraz wokół niej
+                        if ((x == 0 && y == 0) || (x == 0 && y == 1) || (x == 1 && y == 0) ||
+                            (x == szerokoscMapy - 1 && y == 0) || (x == 0 && y == wysokoscMapy - 1) ||
+                            (x == szerokoscMapy - 1 && y == wysokoscMapy - 1))
+                        {
+                            teren = LAKA; // Zmieniamy skalę na łąkę w tych miejscach
+                        }
+
+                        if (teren == SKALA)
+                        {
+                            // Zapobiegamy umieszczaniu skał na pozycji (0, 0) oraz wokół niej
+                            if (x > 0 && y > 0 && x < szerokoscMapy - 1 && y < wysokoscMapy - 1)
+                            {
+                                mapa[y, x] = SKALA;
+                            }
+                            else
+                            {
+                                mapa[y, x] = LAKA; // Zmieniamy na łąkę, jeśli na brzegach mapy
+                            }
+                        }
+                        else
+                        {
+                            mapa[y, x] = teren;
+                        }
                     }
                 }
 
-                // Przygotowanie kontenera SiatkaMapy – czyszczenie elementów i definicji wierszy/kolumn
                 SiatkaMapy.Children.Clear();
                 SiatkaMapy.RowDefinitions.Clear();
                 SiatkaMapy.ColumnDefinitions.Clear();
@@ -85,12 +109,12 @@ namespace Gra2D
                 {
                     SiatkaMapy.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(RozmiarSegmentu) });
                 }
+
                 for (int x = 0; x < szerokoscMapy; x++)
                 {
                     SiatkaMapy.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(RozmiarSegmentu) });
                 }
 
-                // Tworzenie tablicy kontrolk Image i dodawanie ich do siatki
                 tablicaTerenu = new Image[wysokoscMapy, szerokoscMapy];
                 for (int y = 0; y < wysokoscMapy; y++)
                 {
@@ -105,7 +129,7 @@ namespace Gra2D
                         int rodzaj = mapa[y, x];
                         if (rodzaj >= 1 && rodzaj < ILE_TERENOW)
                         {
-                            obraz.Source = obrazyTerenu[rodzaj];//wczytanie obrazka terenu
+                            obraz.Source = obrazyTerenu[rodzaj];
                         }
                         else
                         {
@@ -114,48 +138,46 @@ namespace Gra2D
 
                         Grid.SetRow(obraz, y);
                         Grid.SetColumn(obraz, x);
-                        SiatkaMapy.Children.Add(obraz);//dodanie obrazka do siatki na ekranie
+                        SiatkaMapy.Children.Add(obraz);
                         tablicaTerenu[y, x] = obraz;
                     }
                 }
 
-                // Dodanie obrazka gracza – ustawiamy go na wierzchu
                 SiatkaMapy.Children.Add(obrazGracza);
-                Panel.SetZIndex(obrazGracza, 1);//ustawienie obrazka gracza na wierzchu
+                Panel.SetZIndex(obrazGracza, 1);
                 pozycjaGraczaX = 0;
                 pozycjaGraczaY = 0;
                 AktualizujPozycjeGracza();
 
                 iloscDrewna = 0;
+                iloscDiamentow = 0; // Resetowanie licznika diamentów
                 EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
-            }//koniec try
+                EtykietaDiamenty.Content = "Diamenty: " + iloscDiamentow; // Dodanie etykiety dla diamentów
+            }
             catch (Exception ex)
             {
                 MessageBox.Show("Błąd wczytywania mapy: " + ex.Message);
             }
         }
 
-        // Aktualizuje pozycję obrazka gracza w siatce
         private void AktualizujPozycjeGracza()
         {
             Grid.SetRow(obrazGracza, pozycjaGraczaY);
             Grid.SetColumn(obrazGracza, pozycjaGraczaX);
         }
 
-        // Obsługa naciśnięć klawiszy – ruch gracza oraz wycinanie lasu
         private void OknoGlowne_KeyDown(object sender, KeyEventArgs e)
         {
             int nowyX = pozycjaGraczaX;
             int nowyY = pozycjaGraczaY;
-            //zmiana pozycji gracza
+
             if (e.Key == Key.Up) nowyY--;
             else if (e.Key == Key.Down) nowyY++;
             else if (e.Key == Key.Left) nowyX--;
             else if (e.Key == Key.Right) nowyX++;
-            //Gracz nie może wyjść poza mapę
+
             if (nowyX >= 0 && nowyX < szerokoscMapy && nowyY >= 0 && nowyY < wysokoscMapy)
             {
-                // Gracz nie może wejść na pole ze skałami
                 if (mapa[nowyY, nowyX] != SKALA)
                 {
                     pozycjaGraczaX = nowyX;
@@ -164,32 +186,65 @@ namespace Gra2D
                 }
             }
 
-            // Obsługa wycinania lasu – naciskamy klawisz C
             if (e.Key == Key.C)
             {
-                if (mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)//jeśli gracz stoi na polu lasu
+                if (mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)
                 {
                     mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
                     tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
                     iloscDrewna++;
                     EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
                 }
+
+                if (mapa[pozycjaGraczaY, pozycjaGraczaX] == DIAMENT)
+                {
+                    mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
+                    tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
+                    iloscDiamentow++;
+                    EtykietaDiamenty.Content = "Diamenty: " + iloscDiamentow;
+
+                    // Jeżeli gracz zebrał odpowiednią liczbę diamentów
+                    if ((wybranyRozmiar == "mala" && iloscDiamentow >= 1) ||
+                        (wybranyRozmiar == "srednia" && iloscDiamentow >= 2) ||
+                        (wybranyRozmiar == "duza" && iloscDiamentow >= 3))
+                    {
+                        MessageBox.Show("Gratulacje! Zebrano wystarczającą liczbę diamentów. Masz teraz umiejętność przechodzenia przez skały!");
+                    }
+                }
             }
         }
 
-        // Obsługa przycisku "Wczytaj mapę"
         private void WczytajMape_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog oknoDialogowe = new OpenFileDialog();
-            oknoDialogowe.Filter = "Plik mapy (*.txt)|*.txt";
-            oknoDialogowe.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory; // Ustawienie katalogu początkowego
-            bool? czyOtwartoMape = oknoDialogowe.ShowDialog();
-            if (czyOtwartoMape == true)
+            string folder = AppDomain.CurrentDomain.BaseDirectory;
+            string plik = Path.Combine(folder, $"{wybranyRozmiar}_{trudnosc}.txt");
+
+            if (File.Exists(plik))
             {
-                WczytajMape(oknoDialogowe.FileName);
+                WczytajMape(plik);
+            }
+            else
+            {
+                MessageBox.Show($"Nie znaleziono mapy: {plik}");
+            }
+        }
+
+        private void WybierzRozmiar_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                wybranyRozmiar = button.Tag.ToString();
+            }
+        }
+
+        private void WybierzTrudnosc_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                trudnosc = button.Tag.ToString();
             }
         }
     }
 }
-
-
